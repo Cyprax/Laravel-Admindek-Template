d333ff8e25c3e4782ef7f4d4c771742db9c945d1
toomelette <toomelette@gmail.com>
2019-01-25 09:18:06 +0800

    Favicon Cache

--- resources/views/layouts/admin-master.blade.php
+++ resources/views/layouts/admin-master.blade.php
@@ -3,7 +3,8 @@
   <head>
     <meta charset="utf-8">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
-    <title>SWEP-AFD | Sugar Web Portal</title>
+    <title>AdminLte-Laravel</title>
+    <link rel="shortcut icon" href="{{ asset('favicon.png') }}">
     <meta content="width=device-width, initial-scale=1, maximum-scale=1, user-scalable=no" name="viewport">
     <meta name="csrf-token" content="{{ csrf_token() }}">
 
