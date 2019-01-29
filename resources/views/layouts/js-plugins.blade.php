c8f6a09c01f7d3f42a9d5267b0f82b82dee8e580
toomelette <toomelette@gmail.com>
2019-01-30 07:54:21 +0800

    Home 1

--- resources/views/layouts/js-plugins.blade.php
+++ resources/views/layouts/js-plugins.blade.php
@@ -1,9 +1,9 @@
 
 <script type="text/javascript" src="{{ asset('template2/bower_components/jquery/js/jquery.min.js') }}"></script>
 <script type="text/javascript" src="{{ asset('template2/bower_components/jquery-ui/js/jquery-ui.min.js') }}"></script>
+<script type="text/javascript" src="{{ asset('template2/bower_components/jquery-slimscroll/js/jquery.slimscroll.js') }}"></script>
 <script type="text/javascript" src="{{ asset('template2/bower_components/popper.js/js/popper.min.js') }}"></script>
 <script type="text/javascript" src="{{ asset('template2/bower_components/bootstrap/js/bootstrap.min.js') }}"></script>
-<script type="text/javascript" src="{{ asset('template2/bower_components/jquery-slimscroll/js/jquery.slimscroll.js') }}"></script>
 <script type="text/javascript" src="{{ asset('template2/bower_components/modernizr/js/modernizr.js') }}"></script>
 <script type="text/javascript" src="{{ asset('template2/bower_components/modernizr/js/css-scrollbars.js') }}"></script>
 
@@ -13,7 +13,3 @@
 <script type="text/javascript" src="{{ asset('template2/assets/js/pcoded.min.js') }}"></script>
 <script type="text/javascript" src="{{ asset('template2/assets/js/vertical/vertical-layout.min.js') }}"></script>
 <script type="text/javascript" src="{{ asset('template2/assets/js/script.min.js') }}"></script>
-<script type="text/javascript" src="{{ asset('template2/assets/js/pcoded.min.js') }}"></script>
-<script type="text/javascript" src="{{ asset('template2/assets/js/vertical/menu/menu-hori-fixed.js') }}"></script>
-<script type="text/javascript" src="{{ asset('template2/assets/js/jquery.mCustomScrollbar.concat.min.js') }}"></script>
-<script type="text/javascript" src="{{ asset('template2/assets/js/script.js') }}"></script>
